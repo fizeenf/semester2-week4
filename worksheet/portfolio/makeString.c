881ee71b4d21c204062bec9300e0cf9f04367fc4
fizeenf <lhxz0885@leeds.ac.uk>
2026-03-25 02:23:12 +0000

    wk4

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Fizeen Farooq
+Student ID: 201877255
 */
 
 #include <stdio.h>
@@ -11,9 +11,27 @@ int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
 
+    char buffer[100];
+
+    strcpy(buffer, "");
+
+
+    for (int i = 1; i < argc; i++) {
+
+        strcat(buffer, argv[i]);
+
+        if (i < argc -1) {
+
+            strcat(buffer, " ");
+
+        }
+        
+    }
+
+
     // process the command-line data using appropriate string functions
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+    printf("%s\n", buffer); // use only this single print statement in your submitted code
 
     return 0;
 }
